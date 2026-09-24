3ca1bb5d2171a031b2d02b664d3050c647834a34
tessy-15 <naoki130315@gmail.com>
2026-09-24 17:48:48 +0900

    Unify slide 9 typography and update site titles

--- scriptMP.js
+++ scriptMP.js
@@ -269,9 +269,14 @@ const slideTextMasks = {
   1: [{ left: 9.3, top: 15.0, width: 66.5, height: 5.4 }],
   8: [
     {
-      left: 452 / 1280 * 100, top: 184 / 720 * 100, width: 645 / 1280 * 100, height: 394 / 720 * 100,
+      left: 158 / 1280 * 100, top: 85 / 720 * 100, width: 650 / 1280 * 100, height: 48 / 720 * 100,
       className: 'slide-text-mask',
-      html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 645 394" width="100%" height="100%" style="display:block">
+      html: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 48" width="100%" height="100%" style="display:block"><text x="6" y="39" fill="#000000" font-family="Yu Gothic, Yu Gothic UI, sans-serif" font-size="37.333333" font-weight="400">Sさん 宮城県在住 48歳 会社員 男性</text></svg>'
+    },
+    {
+      left: 452 / 1280 * 100, top: 184 / 720 * 100, width: 650 / 1280 * 100, height: 402 / 720 * 100,
+      className: 'slide-text-mask',
+      html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 402" width="100%" height="100%" style="display:block">
         <text fill="#000000" font-family="Yu Gothic, Yu Gothic UI, sans-serif" font-size="37.333333" font-weight="400"><tspan x="0" y="32">居間でテレビを見ていたら、突然、テ</tspan><tspan x="0" y="77">レビとスマホから緊急地震速報が流れ</tspan><tspan x="0" y="122">てきました。</tspan><tspan x="0" y="186">どうしたらよいか分からないまま</tspan><tspan x="0" y="231">何もできず、大きな揺れが来るのを</tspan><tspan x="0" y="276">待つだけでした。</tspan><tspan x="0" y="344">緊急地震速報は役に立たないと実感</tspan><tspan x="0" y="389">しました。</tspan></text>
       </svg>`
     },
